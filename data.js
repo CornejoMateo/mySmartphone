28210bf7f42102803fc3155a0137089e5eded24e
Mateo Cornejo <ccornejomateo@gmail.com>
2025-06-02 08:58:53 -0300

    Se agrego un modal para cuando se presione un producto, asi te muestra los detalles, ademas se agregaron los estilos correspondientes

--- data.js
+++ data.js
@@ -1,27 +1,14 @@
-window.relojes = [
+window.dispositivos = [
   {
     nombre: "Reloj Noga Smartwach BT SW04",
     precio: "$19.990",
-    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
-  },
-  {
-    nombre: "Reloj Xiaomi Redmi Watch 3 Active",
-    precio: "$35.999",
-    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
-  },
-  {
-    nombre: "Reloj Xiaomi Redmi Watch 3 Active",
-    precio: "$35.999",
-    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
-  },
-  {
-    nombre: "Reloj Xiaomi Redmi Watch 3 Active",
-    precio: "$35.999",
-    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
-  },
-  {
-    nombre: "Reloj Xiaomi Redmi Watch 3 Active",
-    precio: "$35.999",
-    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
+    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc",
+    descripcion: "Smartwatch elegante con conectividad Bluetooth y monitor de actividad.",
+    detalles: [
+      "Pantalla táctil de 1.4 pulgadas",
+      "Compatible con Android e iOS",
+      "Duración de batería: 7 días",
+      "Control de música y notificaciones"
+    ]
   },
 ];
