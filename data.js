587015456cef3a81a02cbb188b2f6ca6d3fa578b
Mateo Cornejo <ccornejomateo@gmail.com>
2025-06-02 08:34:44 -0300

    Archivo que va a traer de base de datos los productos

---
+++ data.js
@@ -0,0 +1,27 @@
+window.relojes = [
+  {
+    nombre: "Reloj Noga Smartwach BT SW04",
+    precio: "$19.990",
+    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
+  },
+  {
+    nombre: "Reloj Xiaomi Redmi Watch 3 Active",
+    precio: "$35.999",
+    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
+  },
+  {
+    nombre: "Reloj Xiaomi Redmi Watch 3 Active",
+    precio: "$35.999",
+    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
+  },
+  {
+    nombre: "Reloj Xiaomi Redmi Watch 3 Active",
+    precio: "$35.999",
+    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
+  },
+  {
+    nombre: "Reloj Xiaomi Redmi Watch 3 Active",
+    precio: "$35.999",
+    imagen: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF1nr94ukKG6_9pi0ALv-azI_wFD9NIjZJj3De2kBatwjEOaDqwBAoDoEHWp7baJqCo3a-1XvJ6gXBY9SKSXBdc9X6s7kAjtWr5WRxSBjTpd29BJW4rsSa2oJ8WEjO9R0XXgtYk_c&usqp=CAc"
+  },
+];
